a5466d93ed372c1d02607db81914e37d72d69cd0
Mianhassanali <32090479+Mianhassanali@users.noreply.github.com>
2018-04-29 11:53:55 +0500

    updated thread.c
    solved problem of updating passed parameter to the thread. The value of parameter change so it changed for every thread because it was a single variable passed by reference so i corrected this problem by using array.

--- thread.c
+++ thread.c
@@ -10,28 +10,28 @@ pthread_t thread[10];
 
 void * add(void* arg)
 {
+	//derefrencing the value of parameter.
 	int start = *((int *)arg);
-	int end = *((int *)arg) + 100; //derefrencing the value of parameter.
-	printf("%d\n",end);
+	int end = *((int *)arg) + 100;
+	printf("Start index = %d\n",start);
+	//Loop to add array values and storing sum in global variable.
 	for(start; start < end; start++)
-		sum += arr[start];  //Adding in global variable.
+		sum += arr[start]; 
 }
 
 int main()
 {
 	for(int i=0; i<1000; i++)
         {
-		arr[i] = 1;
+		arr[i] = i+1;
 	}
 	
 	
-	int j=0;
+	int indexes[10] = {0,100,200,300,400,500,600,700,800,900};
 
-	for(int i=0; i<1000; i+=100)
+	for(int i=0; i<10; i++)
 	{
-		int x=i;
-		pthread_create(&thread[j],NULL,add,&x);
-		j++;
+		pthread_create(&thread[i],NULL,add,&indexes[i]);
 	}
 	
 
