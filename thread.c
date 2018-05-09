258cb8e16eecb68ccbb2a88d86bfb1b86c7fad15
Mianhassanali <32090479+Mianhassanali@users.noreply.github.com>
2018-05-09 22:54:04 +0500

    Final commit
    In this commit i updated the function passed to thread to return the local sum instead to adding in global variable. Every thread returned its own sum in main i added all the returned values and displayed the sum of whole array.

--- thread.c
+++ thread.c
@@ -2,43 +2,44 @@
 #include<pthread.h>
 #include<stdlib.h>
 
-
-int sum = 0;
 int arr[1000];
-pthread_t thread[10];
 
+void * add(void* arg);
+
+int main()
+{
+	int *sum;           	     //Pointer to receive sum returned by a thread
+	int tsum = 0;                //Variable to sum values returned by threads
+	pthread_t thread[10];
+	
+	for(int i=0; i<1000; i++)    //Array initializing loop 
+		arr[i] = i+1;	
+	
+	int indexes[10] = {0,100,200,300,400,500,600,700,800,900};
+
+	for(int i=0; i<10; i++)      //Thread creation loop
+		pthread_create(&thread[i],NULL,add,&indexes[i]);
+	
+	for(int i=0; i<10; i++)	     //Loop to join all the threads to the main process
+	{
+		pthread_join(thread[i],(void **)&sum);
+		tsum += *sum;
+		free(sum);
+	}
+
+	printf("The sum of array is %d\n", tsum);
+	return 0;
+}
 
 void * add(void* arg)
 {
 	//derefrencing the value of parameter.
 	int start = *((int *)arg);
 	int end = *((int *)arg) + 100;
-	printf("Start index = %d\n",start);
-	//Loop to add array values and storing sum in global variable.
+	int *sum = (int *)malloc(sizeof(int)); //Creating local sum parameter
+		
 	for(start; start < end; start++)
-		sum += arr[start]; 
-}
-
-int main()
-{
-	for(int i=0; i<1000; i++)
-        {
-		arr[i] = i+1;
-	}
+		*sum += arr[start]; 
 	
-	
-	int indexes[10] = {0,100,200,300,400,500,600,700,800,900};
-
-	for(int i=0; i<10; i++)
-	{
-		pthread_create(&thread[i],NULL,add,&indexes[i]);
-	}
-	
-
-	for(int i=0; i<10; i++)
-		pthread_join(thread[i], NULL);
-
-	printf("The sum is %d\n",sum);
-
-	return 0;
+	return (void *)sum;
 }
